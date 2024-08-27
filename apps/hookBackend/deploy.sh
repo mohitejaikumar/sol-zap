58a8ab1144bece3d907f35e6820d13ad3b83bc28
mohitejaikumar <mohitjaikumar@gmail.com>
2024-08-27 14:17:52 +0530

    changed node version

--- apps/hookBackend/deploy.sh
+++ apps/hookBackend/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
 cd /home/ubuntu/sol-zap
 git pull origin main
